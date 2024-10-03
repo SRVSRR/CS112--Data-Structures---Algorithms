3ae700a81f61e22c33be16ed537d938dc0864f9a
Rohan Nandan <rohannandan2004@gmail.com>
2024-10-03 15:44:22 +1200

    Feat : Finished Wk10 Labs

--- wk10s1.cpp
+++ wk10s1.cpp
@@ -5,147 +5,102 @@
 
 using namespace std;
 
+// Student struct
 struct Student {
-   string Id;
-   double marks;
-   char grade;
+    string id;
+    int marks;
+    char grade;
 };
 
+// NODE pointing to student struct
 struct NODE {
-   NODE *pNext;
-   NODE *pPrev;
-   int nData;
+    NODE *pNext;
+    NODE *pPrev;
+    Student *studentData; // Pointer to Student object
 };
 
-//declare head and tail of list
-//originally the list is empty
-NODE *pHead=NULL, *pTail=NULL;
+NODE *pHead = NULL, *pTail = NULL;
 
+// Prototypes
 void AppendNode(NODE *pNode);
 void InsertNode(NODE *pNode, NODE *pAfter);
 void RemoveNode(NODE *pNode);
-void DeleteAllNodes( );
+void DeleteAllNodes();
 void discard_line(ifstream &in);
 
-int main( )
-{
-   NODE *pNode;
-   for (int i = 0; i < 10; i++){
-        pNode = new NODE;    //allocate
-	                        // memory for each node and make
-                            // pointer point to the
-                            //dynamically allocated memory
-    	pNode->nData = i;  //put some data in the node
-        AppendNode(pNode); //add node to list
+// Determine Grade
+char calculateGrade(int marks) {
+    if (marks >= 85) return 'A';
+    if (marks >= 70) return 'B';
+    if (marks >= 50) return 'C';
+    if (marks >= 30) return 'D';
+    return 'F';
+}
+
+int main() {
+    NODE *pNode;
+    
+    // Read file
+    ifstream readFile("wk10_students.txt"); 
+
+    string discardLine;
+    getline(readFile, discardLine); // discard first line
+    
+    while (!readFile.eof()) {
+        string id;
+        int marks;
+        
+        readFile >> id >> marks;
+
+        // Create a new student object
+        Student* newStudent = new Student;
+        newStudent->id = id;
+        newStudent->marks = marks;
+        newStudent->grade = calculateGrade(marks);
+
+        // Create a new node to store this student
+        pNode = new NODE;
+        pNode->studentData = newStudent;
+        
+        // Append node to the linked list
+        AppendNode(pNode);
     }
 
-    // Now display each item in list
-   for(pNode = pHead; pNode != NULL; pNode = pNode->pNext){
-      cout<<pNode->nData<<endl;
-   }
+    readFile.close();
 
-   DeleteAllNodes();
-
-    cout<<"Displaying afgter delete"<<endl;
-   for(pNode = pHead; pNode != NULL; pNode = pNode->pNext){
-      cout<<pNode->nData<<endl;
-   }
-
-   /*
-    NODE * pNode;
-
-    //Add items to linked list
-    for (int i = 0; i < 10; i++){
-        pNode = new NODE;    //allocate
-	                        // memory for each node and make
-                            // pointer point to the
-                            //dynamically allocated memory
-    	pNode->nData = i;  //put some data in the node
-        AppendNode(pNode); //add node to list
+    // Print student data (ID and marks)
+    for (pNode = pHead; pNode != NULL; pNode = pNode->pNext) {
+        cout << "Student ID: " << pNode->studentData->id
+             << ", Marks: " << pNode->studentData->marks
+             << ", Grade: " << pNode->studentData->grade << endl;
     }
-
-    // Now display each item in list
-    for(pNode = pHead; pNode != NULL; pNode = pNode->pNext)
-        cout<<pNode->nData<<endl;
 
     DeleteAllNodes();
-
-    cout<<"Displaying afgter delete"<<endl;
-    for(pNode = pHead; pNode != NULL; pNode = pNode->pNext)
-        cout<<pNode->nData<<endl;
-*/
-    system ("pause");
-    return 0;
 }
 
-//function Implementations
-
-void AppendNode(NODE *pNode)
-{
-   if (pHead == NULL) {     //if list is empty
-      pHead = pNode;        //make head point to pNode
-      pNode->pPrev = NULL;
-   }
-   else {
-      pTail->pNext = pNode;  //make tail point to pNode
-      pNode->pPrev = pTail;
-   }
-   pTail = pNode;        //tail is now pNode
-   pNode->pNext = NULL;  //pNode next now points to NULL
+// Appends a node at the end of the list
+void AppendNode(NODE *pNode) {
+    if (pHead == NULL) {
+        // List is empty
+        pHead = pTail = pNode;
+        pNode->pNext = pNode->pPrev = NULL;
+    } else {
+        // Append at the end
+        pTail->pNext = pNode;
+        pNode->pPrev = pTail;
+        pNode->pNext = NULL;
+        pTail = pNode;
+    }
 }
 
-// Inserts a node into the list after pAfter
-void InsertNode(NODE *pNode, NODE *pAfter)
-{
-   pNode->pNext = pAfter->pNext; //make next of new node point to "next" node
-   pNode->pPrev = pAfter;               //make prev of new node point the "after" node
-
-   if (pAfter->pNext != NULL)       // if we are NOT inserting at the end
-      pAfter->pNext->pPrev = pNode;    //make prev of  "next" node point to the new node
-   else
-      pTail = pNode;                      //if we are inserting at the end, make new node the tail
-
-   pAfter->pNext = pNode;   //make next of  "after" node point to new node
+// Deletes all nodes in the list
+void DeleteAllNodes() {
+    NODE *pTemp = pHead;
+    while (pTemp != NULL) {
+        NODE *pDel = pTemp;
+        pTemp = pTemp->pNext;
+        delete pDel->studentData; // Free memory for the student object
+        delete pDel;              // Free memory for the node
+    }
+    pHead = pTail = NULL;
 }
-
-// Removes the specified node from the list
-void RemoveNode(NODE *pNode)
-{
-   if (pNode->pPrev == NULL)  //if removing the head
-       pHead = pNode->pNext;
-
-   else
-      pNode->pPrev->pNext = pNode->pNext;  //if removing a middle node
-
-   if (pNode->pNext == NULL)  //if removing the tail
-      pTail = pNode->pPrev;
-       
-   else
-      pNode->pNext->pPrev = pNode->pPrev;
-
-   delete pNode;  //*free the memory
-}
-
-//****** Deletes the entire list******
-void DeleteAllNodes( )
-{
-   while (pHead != NULL)   //keep on removing until the 
-                           //head points to NULL
-      RemoveNode(pHead);
-}
-
-void discard_line(ifstream &in)
-{
-    char c;
-
-    do
-   	    in.get(c);
-    while (c!='\n');
-}
-
-
-
-
-
-
-

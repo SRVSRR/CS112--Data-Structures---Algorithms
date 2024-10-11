2a0cd6708be772bb2b894cbbd1fa09504535409c
Rohan Nandan <rohannandan2004@gmail.com>
2024-10-11 13:05:06 +1200

    f

---
+++ linkedLists.cpp
@@ -0,0 +1,46 @@
+#include <iostream> 
+
+using namespace std;
+
+struct node {
+    public:
+        int data;
+        node *next;
+};
+
+class stack {
+    public:
+        node *head;
+        void push(node *node)
+        {
+            if(head != NULL){
+                head->next = node;
+            }
+            head = node;
+        }
+        void pop(node *node){
+            if(head ){
+                head->next = NULL;
+            }
+            head = NULL;
+        }
+};
+
+int main(){
+    node *n = new node;
+    n->data  = 5;
+    n->next  = NULL;
+
+    node *topnode = new node;
+    topnode->data = 2;
+    topnode->next = NULL;
+
+
+    node *n = new node;
+    n->data  = 5;
+    n->next  = NULL;
+
+ 
+
+    return 0;
+}

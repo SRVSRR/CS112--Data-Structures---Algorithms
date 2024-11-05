7cb414bd47bde47218afe21ba450d23b8bc4fd2f
Rohan Nandan <rohannandan2004@gmail.com>
2024-11-05 17:36:48 +1200

    Fix: allocate and deallocate dynamic memory

--- FInalsSolutions/Polymorphism.cpp
+++ FInalsSolutions/Polymorphism.cpp
@@ -6,12 +6,14 @@ class employee{
     public:
         employee(){};
         ~employee(){};
-        void setname(string name){
+        string setname(string name){
             this->name = name;
+            return this->name;
         }
 
-        void setID(string ID){
+        string setID(string ID){
             this->ID = ID;
+            return this->ID;
         }
 
         string getname() {
@@ -89,39 +91,40 @@ void print_salary(employee *em){
 }
 
 int main(){
-    hourly_employee em1;
-    em1.setname("Rohan");
-    em1.setID("001");
-    em1.sethourlyRate(1.2);
-    em1.sethoursWorked(12);
+    hourly_employee* em1 = new hourly_employee();
+    em1->setname("Rohan");
+    em1->setID("001");
+    em1->sethourlyRate(1.2);
+    em1->sethoursWorked(12);
 
-    cout << "Name: " << em1.getname() << endl;
-    cout << "ID: " << em1.getID() << endl;
-    cout << "Hourly Rate: " << em1.gethourlyRate() << endl;
-    cout << "Hours Worked: " << em1.gethoursWorked() << endl;
-    cout << "Salary : " << em1.calc_salary() << endl;
+    cout << "Name: " << em1->getname() << endl;
+    cout << "ID: " << em1->getID() << endl;
+    cout << "Hourly Rate: " << em1->gethourlyRate() << endl;
+    cout << "Hours Worked: " << em1->gethoursWorked() << endl;
+    cout << "Salary: " << em1->calc_salary() << endl;
 
     cout << endl;
 
-    Commision_Employee em2;
-    em2.setname("Aisha");
-    em2.setID("002");
-    em2.setTotalSales(5000);
-    em2.setCommissionPercent(0.10);
+    Commision_Employee* em2 = new Commision_Employee();
+    em2->setname("Aisha");
+    em2->setID("002");
+    em2->setTotalSales(5000);
+    em2->setCommissionPercent(0.10);
 
-    cout << "Name: " << em2.getname() << endl;
-    cout << "ID: " << em2.getID() << endl;
-    cout << "Commission Rate: " << em2.getCommissionPercent() << endl;
-    cout << "Total Sales : " << em2.getTotalSales() << endl;
-    cout << "Salary : " << em2.calc_salary() << endl;
+    cout << "Name: " << em2->getname() << endl;
+    cout << "ID: " << em2->getID() << endl;
+    cout << "Commission Rate: " << em2->getCommissionPercent() << endl;
+    cout << "Total Sales: " << em2->getTotalSales() << endl;
+    cout << "Salary: " << em2->calc_salary() << endl;
 
     cout << endl;
 
     cout << "Salary Stats" << endl;
-    cout << em1.getname() << ": "; print_salary(&em1);
-    cout << endl;
-    cout << em2.getname() << ": "; print_salary(&em2);
+    cout << em1->getname() << ": "; print_salary(em1);
+    cout << em2->getname() << ": "; print_salary(em2);
 
+    delete em1;
+    delete em2;
 
     return 0;
 }
